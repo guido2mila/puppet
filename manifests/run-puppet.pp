663728751b407001342b50bcc2b41790d3cc1c5a
guido <guido2mila@gmail.com>
2019-04-26 11:55:34 +0200

    aggiunto utente guido con chiave

--- manifests/run-puppet.pp
+++ manifests/run-puppet.pp
@@ -19,3 +19,16 @@ package { 'puppet-lint':
   provider => gem,
 }
 
+user { 'guido':
+  ensure => present,
+  uid    => '3001',
+  home   => '/home/guido',
+  shell  => '/bin/bash',
+  groups => ['devs'],
+}
+
+ssh_authorized_key { 'guizzunti':
+  user => 'guido',
+  type => 'ssh-dss',
+  key  => 'AAAAB3NzaC1kc3MAAACBALVspOaAJ+SLJUt8vVfDGfllpUNMObc/HCAFk/HJjbg5MnVDKcMSnukhetD+40CCZooNp/S7gt4X9yVGFUq2esR/pf3XWixhrNSltD/ndmpHdkTXsaXqykBl6hKGZA4UwAvpFn20Ucbr7suTHRFTvo9rNawmnb5CJWbracsW7W6BAAAAFQDAb882CknfZWP+DSzAvTOJBg2TnQAAAIB+EwN9nw9j9v5IsQyJ+jDmwDK8U0z8Cwbl9htnq4CugP9wfrK4IcAibUvBnKMqd1scdarVrP35U0wOKmkH1yyjr94ymDvTQuxWGa6akl1LVEU2kqtDZ/ozknvw5eiTtySDACVArPNTX+pTSe0G1VJU0FaeVR5jvlmELVRVODDUOAAAAIEAj3rEkf9eFfemgFbAoWoPF3lc6WMr463MccHUbIbRV+mOdGWKm9nTMkZWJg4AVx3cmeSUy5a9P0H5Y4UvOwJWuhlKa8PpB74HCCLPrVOtkxKEm6UURZwR5dG8TCteqwxLd+Bg4t06zIpADf2GLGjn/G1jAAOup2oluCax/kJZ7NU=',
+}
